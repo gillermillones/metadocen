a0c095d7685cbb1b887911efdc6cb5b3e86a14b5
Guillermo <159314465+gillermillones@users.noreply.github.com>
2026-07-30 10:40:11 +0200

    css adjustments

--- app/ui/items/create-form.tsx
+++ app/ui/items/create-form.tsx
@@ -372,14 +372,14 @@ export default function ItemForm() {
             </form>
             <div className="flex flex-col justify-start">
                 <section className="container">
-                    <div {...getRootBigProps({ className: "flex flex-col justify-center px-16 py-10 mx-2 mb-4 border border-dashed rounded-2xl border-black hover:border-gray-500" })}>
+                    <div {...getRootBigProps({ className: "flex flex-col justify-center px-8 py-8 mx-2 mb-4 border border-dashed rounded-2xl border-black hover:border-gray-500" })}>
                         <input {...getInputBigProps()} />
                         <p>Arrastra aquí el archivo XML con metadatos o haz click para seleccionar</p>
                     </div>
                 </section>
-                <p>-------------------- o --------------------</p>
+                <p>------------- o -------------</p>
                 <section className="container2">
-                    <div {...getRootLilProps({ className: "flex flex-col justify-center px-16 py-10 mx-2 my-4 border border-dashed rounded-2xl border-black hover:border-gray-500" })}>
+                    <div {...getRootLilProps({ className: "flex flex-col justify-center px-8 py-8 mx-2 my-4 border border-dashed rounded-2xl border-black hover:border-gray-500" })}>
                         <input {...getInputLilProps()} />
                         <p>Arrastra aquí el documento sobre el que quieres los metadatos o haz click para seleccionar</p>
                     </div>
--- app/ui/items/form-options.tsx
+++ app/ui/items/form-options.tsx
@@ -21,7 +21,7 @@ export default function FormOptions({ field, num, numOpt, color }: { field: stri
                 Elige el valor {field}
             </legend>
             <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-                <div className="flex gap-4" aria-describedby={`${field}-error`}>
+                <div className="flex gap-4 justify-center" aria-describedby={`${field}-error`}>
                     {elem.map((n) => (
                         <div className="flex items-center">
                             <input
@@ -35,7 +35,7 @@ export default function FormOptions({ field, num, numOpt, color }: { field: stri
                             />
                             <label
                                 htmlFor={`${field}-${n}`}
-                                className={`ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-${color}-${colorVal[Math.floor((colorVal.length/2)-(numOpt/2)) + n]} px-3 py-1.5 text-xs font-medium text-white`}
+                                className={`ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-${color}-${colorVal[Math.floor((colorVal.length/2)-(numOpt/2)) + (n - 1)]} px-3 py-1.5 text-xs font-medium text-white`}
                             >
                                 {n}
                             </label>
